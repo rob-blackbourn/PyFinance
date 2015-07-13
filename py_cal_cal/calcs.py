a7c12346cd19edc2d4f3ec16e996ece303d1fa78
rob-blackbourn <rob.blackbourn@googlemail.com>
2015-07-13 09:42:20 +0100

    Put some coordinate systems into classes

--- py_cal_cal/calcs.py
+++ py_cal_cal/calcs.py
@@ -1879,81 +1879,89 @@ class BalineseDate(object):
 ######################
 # Time and Astronomy #
 ######################
-def ecliptical_from_equatorial(ra, declination, obliquity):
-    """Convert equatorial coordinates (in degrees) to ecliptical ones.
-    'declination' is the declination,
-    'ra' is the right ascension and
-    'obliquity' is the obliquity of the ecliptic.
-    NOTE: if 'apparent' right ascension and declination are used, then 'true'
-          obliquity should be input.
-    """
-    co = cos_degrees(obliquity)
-    so = sin_degrees(obliquity)
-    sa = sin_degrees(ra)
-    lon = normalized_degrees_from_radians(
-        atan2(sa*co + tan_degrees(declination)*so, cos_degrees(ra)))
-    lat = arcsin_degrees(
-            sin_degrees(declination)*co -
-            cos_degrees(declination)*so*sa)
-    return [lon, lat]
 
-def equatorial_from_ecliptical(longitude, latitude, obliquity):
-    """Convert ecliptical coordinates (in degrees) to equatorial ones.
-    'longitude' is the ecliptical longitude,
-    'latitude'  is the ecliptical latitude and
-    'obliquity' is the obliquity of the ecliptic.
-    NOTE: resuting 'ra' and 'declination' will be referred to the same equinox
-          as the one of input ecliptical longitude and latitude.
-    """
-    co = cos_degrees(obliquity)
-    so = sin_degrees(obliquity)
-    sl = sin_degrees(longitude)
-    ra = normalized_degrees_from_radians(
-        atan2(sl*co - tan_degrees(latitude)*so,
-        cos_degrees(longitude)))
-    dec = arcsin_degrees(
-            sin_degrees(latitude)*co +
-            cos_degrees(latitude)*so*sl)
-    return [ra, dec]
+class ElipticalCoordinates(object):
+    
+    def __init__(self, longitude, latitude):
+        self.longitude = longitude
+        self.latitude = latitude
 
-def horizontal_from_equatorial(H, declination, latitude):
-    """Convert equatorial coordinates (in degrees) to horizontal ones.
-    Return 'azimuth' and 'altitude'.
-    'H'            is the local hour angle,
-    'declination'  is the declination,
-    'latitude'     is the observer's geographic latitude.
-    NOTE: 'azimuth' is measured westward from the South.
-    NOTE: This is not a good formula for using near the poles.
-    """
-    ch = cos_degrees(H)
-    sl = sin_degrees(latitude)
-    cl = cos_degrees(latitude)
-    A = normalized_degrees_from_radians(
-            atan2(sin_degrees(H), 
-                  ch * sl - tan_degrees(declination) * cl))
-    h = arcsin_degrees(sl * sin_degrees(declination) + 
-                       cl * cos_degrees(declination) * ch)
-    return [A, h]
+    @classmethod        
+    def from_equatorial(cls, ra, declination, obliquity):
+        """Convert equatorial coordinates (in degrees) to ecliptical ones.
+        'declination' is the declination,
+        'ra' is the right ascension and
+        'obliquity' is the obliquity of the ecliptic.
+        NOTE: if 'apparent' right ascension and declination are used, then 'true'
+              obliquity should be input.
+        """
+        co = cos_degrees(obliquity)
+        so = sin_degrees(obliquity)
+        sa = sin_degrees(ra)
+        longitude = normalized_degrees_from_radians(atan2(sa*co + tan_degrees(declination)*so, cos_degrees(ra)))
+        latitude = arcsin_degrees(sin_degrees(declination)*co - cos_degrees(declination)*so*sa)
+        return ElipticalCoordinates(longitude, latitude)
 
-def equatorial_from_horizontal(A, h, phi):
-    """Convert equatorial coordinates (in degrees) to horizontal ones.
-    Return 'local hour angle' and 'declination'.
-    'A'   is the azimuth,
-    'h'   is the altitude,
-    'phi' is the observer's geographical latitude.
-    NOTE: 'azimuth' is measured westward from the South.
-    """
-    H = normalized_degrees_from_radians(
-            atan2(sin_degrees(A), 
-                  (cos_degrees(A) * sin_degrees(phi) + 
-                   tan_degrees(h) * cos_degrees(phi))))
-    delta = arcsin_degrees(sin_degrees(phi) * sin_degrees(h) - 
-                           cos_degrees(phi) * cos_degrees(h) * cos_degrees(A))
-    return [H, delta]
+class EquatorialCoordinates(object):
+    
+    def __init__(self, ra, declination):
+        self.ra = ra
+        self.declination = declination
 
+    @classmethod        
+    def from_ecliptical(cls, longitude, latitude, obliquity):
+        """Convert ecliptical coordinates (in degrees) to equatorial ones.
+        'longitude' is the ecliptical longitude,
+        'latitude'  is the ecliptical latitude and
+        'obliquity' is the obliquity of the ecliptic.
+        NOTE: resuting 'ra' and 'declination' will be referred to the same equinox
+              as the one of input ecliptical longitude and latitude.
+        """
+        co = cos_degrees(obliquity)
+        so = sin_degrees(obliquity)
+        sl = sin_degrees(longitude)
+        ra = normalized_degrees_from_radians(atan2(sl*co - tan_degrees(latitude)*so, cos_degrees(longitude)))
+        dec = arcsin_degrees(sin_degrees(latitude)*co + cos_degrees(latitude)*so*sl)
+        return EquatorialCoordinates(ra, dec)
 
+class HorizontalCoordinates(object):
+    
+    def __init__(self, azimuth, altitude):
+        self.azimuth = azimuth
+        self.altitude = altitude
 
+    @classmethod        
+    def horizontal_from_equatorial(cls, H, declination, latitude):
+        """Convert equatorial coordinates (in degrees) to horizontal ones.
+        Return 'azimuth' and 'altitude'.
+        'H'            is the local hour angle,
+        'declination'  is the declination,
+        'latitude'     is the observer's geographic latitude.
+        NOTE: 'azimuth' is measured westward from the South.
+        NOTE: This is not a good formula for using near the poles.
+        """
+        ch = cos_degrees(H)
+        sl = sin_degrees(latitude)
+        cl = cos_degrees(latitude)
+        azimuth = normalized_degrees_from_radians(atan2(sin_degrees(H), ch * sl - tan_degrees(declination) * cl))
+        altitude = arcsin_degrees(sl * sin_degrees(declination) + cl * cos_degrees(declination) * ch)
+        return HorizontalCoordinates(azimuth, altitude)
 
+    def to_equatorial(self, phi):
+        """Convert equatorial coordinates (in degrees) to horizontal ones.
+        Return 'local hour angle' and 'declination'.
+        'A'   is the azimuth,
+        'h'   is the altitude,
+        'phi' is the observer's geographical latitude.
+        NOTE: 'azimuth' is measured westward from the South.
+        """
+        H = normalized_degrees_from_radians(
+                atan2(sin_degrees(self.azimuth), 
+                      (cos_degrees(self.azimuth) * sin_degrees(phi) + 
+                       tan_degrees(self.altitude) * cos_degrees(phi))))
+        delta = arcsin_degrees(sin_degrees(phi) * sin_degrees(self.altitude) - 
+                               cos_degrees(phi) * cos_degrees(self.altitude) * cos_degrees(self.azimuth))
+        return [H, delta]
 
 def julian_centuries(tee):
     """Return Julian centuries since 2000 at moment tee."""
@@ -1985,13 +1993,6 @@ def precise_obliquity(tee):
                      angle(0, 0, mpf(+   5.79)),
                      angle(0, 0, mpf(+   2.45))]))
 
-def true_obliquity(tee):
-    """Return 'true' obliquity of ecliptic at moment tee.
-    That is, where nutation is taken into accout."""
-    pass
-
-
-# see lines 2882-2891 in calendrica-3.0.cl
 def declination(tee, beta, lam):
     """Return declination at moment UT tee of object at
     longitude 'lam' and latitude 'beta'."""
@@ -2000,15 +2001,11 @@ def declination(tee, beta, lam):
         (sin_degrees(beta) * cos_degrees(varepsilon)) +
         (cos_degrees(beta) * sin_degrees(varepsilon) * sin_degrees(lam)))
 
-# see lines 2893-2903 in calendrica-3.0.cl
 def right_ascension(tee, beta, lam):
     """Return right ascension at moment UT 'tee' of object at
     latitude 'lam' and longitude 'beta'."""
     varepsilon = obliquity(tee)
-    return arctan_degrees(
-        (sin_degrees(lam) * cos_degrees(varepsilon)) -
-        (tan_degrees(beta) * sin_degrees(varepsilon)),
-        cos_degrees(lam))
+    return arctan_degrees((sin_degrees(lam) * cos_degrees(varepsilon)) - (tan_degrees(beta) * sin_degrees(varepsilon)), cos_degrees(lam))
 
 class Location(object):
 
