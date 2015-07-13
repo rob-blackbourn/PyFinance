cbbd32d09af51bcdfeb6b8c5c03df015ac79275e
rob-blackbourn <rob.blackbourn@googlemail.com>
2015-07-13 14:19:28 +0100

    Split out the Islamic calendar

--- py_calendrical/py_cal_cal.py
+++ py_calendrical/py_cal_cal.py
@@ -1007,57 +1007,6 @@ def pentecost(g_year):
     return easter(g_year) + 49
 
 
-###############################
-# islamic calendar algorithms #
-###############################
-class IslamicDate(object):
-
-    EPOCH = JulianDate(JulianDate.ce(622), JulianMonth.July, 16).to_fixed()
-
-    def __init__(self, year, month, day):
-        self.year = year
-        self.month = month
-        self.day = day
-
-    @classmethod    
-    def is_leap_year(cls, year):
-        """Return True if year is an Islamic leap year."""
-        return mod(14 + 11 * year, 30) < 11
-    
-    def to_fixed(self):
-        """Return fixed date equivalent to Islamic date i_date."""
-        return (self.EPOCH - 1 +
-                (self.year - 1) * 354  +
-                quotient(3 + 11 * self.year, 30) +
-                29 * (self.month - 1) +
-                quotient(self.month, 2) +
-                self.day)
-
-    @classmethod
-    def from_fixed(cls, date):
-        """Return Islamic date (year month day) corresponding to fixed date date."""
-        year       = quotient(30 * (date - cls.EPOCH) + 10646, 10631)
-        prior_days = date - IslamicDate(year, 1, 1).to_fixed()
-        month      = quotient(11 * prior_days + 330, 325)
-        day        = date - IslamicDate(year, month, 1).to_fixed() + 1
-        return IslamicDate(year, month, day)
-
-    @classmethod
-    def in_gregorian(cls, i_month, i_day, g_year):
-        """Return list of the fixed dates of Islamic month i_month, day i_day that
-        occur in Gregorian year g_year."""
-        jan1  = GregorianDate.new_year(g_year)
-        y     = cls.from_fixed(jan1).year
-        date1 = IslamicDate(y, i_month, i_day).to_fixed()
-        date2 = IslamicDate(y + 1, i_month, i_day).to_fixed()
-        date3 = IslamicDate(y + 2, i_month, i_day).to_fixed()
-        return list_range([date1, date2, date3], GregorianDate.year_range(g_year))
-    
-    @classmethod
-    def mawlid_an_nabi(cls, g_year):
-        """Return list of fixed dates of Mawlid_an_Nabi occurring in Gregorian
-        year g_year."""
-        return cls.in_gregorian(3, 12, g_year)
 
 
 
@@ -2797,31 +2746,6 @@ def phasis_on_or_before(date, location):
            else (mean - 2))
     return  next_int(tau, lambda d: visible_crescent(d, location))
 
-# see lines 5862-5866 in calendrica-3.0.cl
-# see lines 220-221 in calendrica-3.0.errata.cl
-# Sample location for Observational Islamic calendar
-# (Cairo, Egypt).
-ISLAMIC_LOCATION = Location(mpf(30.1), mpf(31.3), 200, Clock.days_from_hours(2))
-
-# see lines 5868-5882 in calendrica-3.0.cl
-def fixed_from_observational_islamic(i_date):
-    """Return fixed date equivalent to Observational Islamic date, i_date."""
-    month    = standard_month(i_date)
-    day      = standard_day(i_date)
-    year     = standard_year(i_date)
-    midmonth = IslamicDate.EPOCH + ifloor((((year - 1) * 12) + month - 0.5) * MEAN_SYNODIC_MONTH)
-    return (phasis_on_or_before(midmonth, ISLAMIC_LOCATION) + day - 1)
-
-# see lines 5884-5896 in calendrica-3.0.cl
-def observational_islamic_from_fixed(date):
-    """Return Observational Islamic date (year month day)
-    corresponding to fixed date, date."""
-    crescent = phasis_on_or_before(date, ISLAMIC_LOCATION)
-    elapsed_months = iround((crescent - IslamicDate.EPOCH) / MEAN_SYNODIC_MONTH)
-    year = quotient(elapsed_months, 12) + 1
-    month = mod(elapsed_months, 12) + 1
-    day = (date - crescent) + 1
-    return IslamicDate(year, month, day)
 
 # see lines 5898-5901 in calendrica-3.0.cl
 JERUSALEM = Location(mpf(31.8), mpf(35.2), 800, Clock.days_from_hours(2))
