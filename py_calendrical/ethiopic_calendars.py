d916afdc681c683469b95b51cf3cfcfac28b6347
rob-blackbourn <rob.blackbourn@googlemail.com>
2015-07-13 14:29:33 +0100

    Split out Ethiopic calendars

---
+++ py_calendrical/ethiopic_calendars.py
@@ -0,0 +1,20 @@
+from py_cal_cal import JulianDate, JulianMonth, CopticDate 
+
+class EthiopicDate(object):
+    
+    EPOCH = JulianDate(JulianDate.ce(8), JulianMonth.August, 29).to_fixed()
+
+    def __init__(self, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
+
+    def to_fixed(self):
+        """Return the fixed date corresponding to Ethiopic date 'e_date'."""
+        return (self.EPOCH + CopticDate(self.year, self.month, self.day).to_fixed() - CopticDate.EPOCH)
+
+    @classmethod
+    def from_fixed(cls, date):
+        """Return the Ethiopic date equivalent of fixed date 'date'."""
+        ymd = CopticDate.from_fixed(date + (CopticDate.EPOCH - cls.EPOCH))
+        return EthiopicDate(ymd.year, ymd.month, ymd.day)
--- py_calendrical/py_cal_cal.py
+++ py_calendrical/py_cal_cal.py
@@ -943,24 +943,6 @@ class CopticDate(object):
         dates in Gregorian year 'g_year'."""
         return cls.coptic_in_gregorian(4, 29, g_year)
 
-class EthiopicDate(object):
-    
-    EPOCH = JulianDate(JulianDate.ce(8), JulianMonth.August, 29).to_fixed()
-
-    def __init__(self, year, month, day):
-        self.year = year
-        self.month = month
-        self.day = day
-
-    def to_fixed(self):
-        """Return the fixed date corresponding to Ethiopic date 'e_date'."""
-        return (self.EPOCH + CopticDate(self.year, self.month, self.day).to_fixed() - CopticDate.EPOCH)
-
-    @classmethod
-    def from_fixed(cls, date):
-        """Return the Ethiopic date equivalent of fixed date 'date'."""
-        ymd = CopticDate.from_fixed(date + (CopticDate.EPOCH - cls.EPOCH))
-        return EthiopicDate(ymd.year, ymd.month, ymd.day)
 
 
 #######################################
