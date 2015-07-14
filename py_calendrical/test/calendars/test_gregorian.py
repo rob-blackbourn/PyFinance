fb5181cb6ed5a9db87ede8e1de050d266ff0ff51
rob-blackbourn <rob.blackbourn@googlemail.com>
2015-07-14 08:31:28 +0100

    Added a test case for Gregorian Put in comparitors in the YearMonthDay base class so the tests work.

--- py_calendrical/test/calendars/test_gregorian.py
+++ py_calendrical/test/calendars/test_gregorian.py
@@ -1,20 +1,29 @@
-'''
-Created on 13 Jul 2015
-
-@author: blackrob
-'''
 import unittest
-from py_calendrical.calendars.gregorian import GregorianDate
 
+from py_calendrical.calendars.gregorian import GregorianDate, JulianMonth
 
-class TestGregorian(unittest.TestCase):
+class GregorianCalendarSmokeTestCase(unittest.TestCase):
 
+    def setUp(self):
+        self.testvalue = 710347
+        self.aDate = GregorianDate(1945, JulianMonth.November, 12)
+        self.myDate = GregorianDate(1967, JulianMonth.January, 30)
+        self.aLeapDate = GregorianDate(1900, JulianMonth.March, 1)
 
-    def testName(self):
-        x = GregorianDate(1967, 8, 12)
-        print x
+    def testConversionFromFixed(self):
+        self.assertEqual(GregorianDate.from_fixed(self.testvalue), self.aDate)
+
+    def testConversionToFixed(self):
+        self.assertEqual(self.testvalue, self.aDate.to_fixed())
+
+    def testLeapYear(self):
+        self.assertTrue(GregorianDate.is_leap_year(2000))
+        self.assertFalse(GregorianDate.is_leap_year(1900))
+
+    def testDayNumber(self):
+        self.assertEqual(self.myDate.day_number(), 30)
+        self.assertEqual(self.aLeapDate.day_number(), 60)
 
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
--- py_calendrical/year_month_day.py
+++ py_calendrical/year_month_day.py
@@ -7,3 +7,22 @@ class YearMonthDay(object):
         
     def __str__(self):
         return "{0}-{1:02}-{2:02}".format(self.year, self.month, self.day)
+    
+    def __eq__(self, other):
+        return isinstance(other, YearMonthDay) and self.year == other.year and self.month == other.month and self.day == other.day
+    
+    def __ne__(self, other):
+        return not self.__eq__(other)
+    
+    def __lt__(self, other):
+        return isinstance(other, YearMonthDay) and (self.years < other.years or self.months < other.months or self.days < other.days)
+    
+    def __le__(self, other):
+        return isinstance(other, YearMonthDay) and (self.years <= other.years or self.months <= other.months or self.days <= other.days)
+    
+    def __gt__(self, other):
+        return isinstance(other, YearMonthDay) and (self.years > other.years or self.months > other.months or self.days > other.days)
+    
+    def __ge__(self, other):
+        return isinstance(other, YearMonthDay) and (self.years >= other.years or self.months >= other.months or self.days >= other.days)
+        
